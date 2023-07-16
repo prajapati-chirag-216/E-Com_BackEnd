0499ef0271b4d5e33989c3ecd4b40698f8939efd
nisha soni <yashusoni003@gmail.com>
2023-07-16 23:40:38 +0530

    we

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
   const allowOrigins = [
   
-   ' https://shopzee.onrender.com',
+   'https://shopzee.onrender.com',
    'https://sz-adminpanel.onrender.com',
    'http://localhost:3000',
    'http://localhost:5000'
